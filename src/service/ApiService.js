57bb33c63c298d6eb882fbe896d753bd79505c8b
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-09-05 10:17:24 +0700

    Update RegisterPage.js

---
+++ src/service/ApiService.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export default class ApiService {
+    static BASE_URL = "http://localhost:8080"
+
+    static getHeader() {
+        const token = localStorage.getItem("token");
+        return {
+            Authorization: `Bearer ${token}`, "Content-Type": "application/json"
+        };
+    }
+
+    /**AUTH */
+
+    /* This  register a new user */
+    static async registerUser(registration) {
+        const response = await axios.post(`${this.BASE_URL}/auth/register`, registration)
+        return response.data
+    }
+
+    /* This  login a registered user */
+    static async loginUser(loginDetails) {
+        const response = await axios.post(`${this.BASE_URL}/auth/login`, loginDetails)
+        return response.data
+    }
+
+    /**AUTHENTICATION CHECKER */
+    static logout() {
+        localStorage.removeItem('token')
+        localStorage.removeItem('role')
+    }
+
+    static isAuthenticated() {
+        const token = localStorage.getItem('token')
+        return !!token
+    }
+
+    static isAdmin() {
+        const role = localStorage.getItem('role')
+        return role === 'ADMIN'
+    }
+
+    static isUser() {
+        const role = localStorage.getItem('role')
+        return role === 'USER'
+    }
+
+}
+
